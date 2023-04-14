5f2d30dfd0a0ea44ee9927da1593571ffa132478
anastasi8989 <125883199+anastasi8989@users.noreply.github.com>
2023-04-14 11:09:08 +0300

    Create palindrome_checker.py

---
+++ palindrome_checker.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+if __name__ == '__main__':
+    user_input = input('Enter a string: ')
+    if is_palindrome(user_input):
+        print(f'{user_input} is a palindrome!')
+    else:
+        print(f'{user_input} is not a palindrome.')
